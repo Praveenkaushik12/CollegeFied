60f6dfdbbb3315e9ddfb39415db6d5688ef4958d
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-03-30 19:11:03 +0530

    database changed

--- collegefied/settings.py
+++ collegefied/settings.py
@@ -89,12 +89,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME":os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "PORT": "5432",
     }
 }
 
