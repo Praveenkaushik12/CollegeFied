07126f501ec19359f67110aefbd4c6d78ee35150
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-03-30 19:25:53 +0530

    updated

--- collegefied/settings.py
+++ collegefied/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "NAME":os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
