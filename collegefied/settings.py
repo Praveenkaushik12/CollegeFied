1cfe27e2bb6e9f3918d9cf7bf4a836d951723aad
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-04-19 02:49:14 +0530

    Update settings.py

--- collegefied/settings.py
+++ collegefied/settings.py
@@ -89,12 +89,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.mysql",
         "NAME":os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "3306",
     }
 }
 
