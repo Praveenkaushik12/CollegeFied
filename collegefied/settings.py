2713146a7c7cf87ccd36bac8b76f44770ad628f4
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-04-19 03:50:35 +0530

    Update settings.py

--- collegefied/settings.py
+++ collegefied/settings.py
@@ -14,9 +14,9 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-a+*ak7@%1fh7f2tk70a#+k^2!tau&o(kn)tg#jw2e^eoemq=(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['collegefied-2.onrender.com', 'localhost', '127.0.0.1','54.87.195.238']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','54.87.195.238']
 
 
 
