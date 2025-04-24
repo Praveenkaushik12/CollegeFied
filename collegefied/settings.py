0262ed003b1ba681111d20203f892144bdddb7b0
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-04-25 01:36:43 +0530

    done

--- collegefied/settings.py
+++ collegefied/settings.py
@@ -112,8 +112,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10 # You can change this to any number
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10 # You can change this to any number
 }
 
 AUTHENTICATION_BACKENDS = [
