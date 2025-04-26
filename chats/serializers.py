4544f8bebd51ec9b3535441a88d923592efa6a57
PraVeen Kaushik <124056241+Praveenkaushik12@users.noreply.github.com>
2025-04-27 01:07:10 +0530

    done

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import ChatRoom, Message
+from api.models import ProductRequest
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,11 +12,13 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     buyer_uname = serializers.SerializerMethodField()
     seller_uname = serializers.SerializerMethodField()
     product_name = serializers.SerializerMethodField()
-    group_name= serializers.SerializerMethodField()
+    group_name = serializers.SerializerMethodField()
+    request_id = serializers.SerializerMethodField()
+
 
     class Meta:
         model = ChatRoom
-        fields = ['id', 'buyer','buyer_uname', 'seller','seller_uname','product','product_name','group_name','created_at', 'is_active','messages']
+        fields = ['id', 'buyer','buyer_uname', 'seller','seller_uname','product','request_id','product_name','group_name','created_at', 'is_active','messages']
 
     def get_buyer_uname(self,obj):
         return obj.buyer.username if obj.buyer else None
@@ -26,8 +29,15 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     def get_product_name(self,obj):
         return obj.product.title if obj.product else None
 
+    def get_request_id(self, obj):
+            request = ProductRequest.objects.filter(product=obj.product).first()
+            if request:
+                return request.id
+            return None
+
     def get_group_name(self, obj):
-        if obj.is_active:
-            return f"chat_{obj.product.id}"
+        if obj.id:  # ChatRoom khud ka id hai
+            return f"chat_{obj.id}"
         return None
+
 
